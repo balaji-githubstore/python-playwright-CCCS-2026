7c9ea579621dc3b0c3f793353a3fedc85ac73e5d
balaji-githubstore <balajidinakaran1@gmail.com>
2026-08-17 09:03:56 +0530

    update

--- demo3_playwright_api/demo1_get_api.py
+++ demo3_playwright_api/demo1_get_api.py
@@ -1,12 +1,12 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    api_context = p.request.new_context()
+    api_context = p.request.new_context(base_url="https://petstore.swagger.io/v2/")
 
     pet_id = 956
 
     response = api_context.get(
-        f"https://petstore.swagger.io/v2/pet/{pet_id}"
+        f"pet/{pet_id}"
     )
 
     # status
